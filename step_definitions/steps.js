8d18c61864640d258e472ad612becc46a621c92a
Mathieu Breton <mathieu.breton@js-republic.com>
2018-04-12 09:18:03 +0200

    cleanup

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,24 +1,14 @@
-const { client } = require('nightwatch-cucumber');
-const { Given, Then, When } = require('cucumber');
+const { client } = require("nightwatch-cucumber");
+const { Given, Then, When } = require("cucumber");
 
-Given(/^I open Google`s search page$/, () => {
-  return client
-    .url('http://google.com');
+Given(/^I open JS-Republic's website$/, () => {
+  return client.url("http://js-republic.com/");
 });
 
-Given(/^I open DuckDuckGo search page$/, () => {
-  return client
-    .url('https://duckduckgo.com/');
-});
-
-Then(/^the title is "(.*?)"$/, (text) => {
+Then(/^the title is "(.*?)"$/, text => {
   return client.assert.title(text);
 });
 
-Then(/^the Google search form exists$/, () => {
-  return client.assert.visible('input[name="q"]');
-});
-
-Then(/^the DuckDuckGo search form exists$/, () => {
-  return client.assert.visible('input[name="q"]');
+Then(/^the JS-Republic logo exist$/, () => {
+  return client.assert.visible(".icon.icon-js-republic");
 });
